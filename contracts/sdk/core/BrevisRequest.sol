13c6c5770a4c347e9c52cc0c92e42510262f7cba
Xiaozhou Li <xli@celer.network>
2024-05-14 19:22:24 -0700

    reorg brevisRequest for op

--- contracts/sdk/core/BrevisRequest.sol
+++ contracts/sdk/core/BrevisRequest.sol
@@ -8,10 +8,6 @@ import "../../interfaces/ISigsVerifier.sol";
 import "../lib/Lib.sol";
 
 contract BrevisRequest is FeeVault {
-    uint256 public requestTimeout;
-    IBrevisProof public brevisProof;
-    ISigsVerifier public immutable sigsVerifier;
-
     enum RequestStatus {
         Pending,
         ZkAttested,
@@ -29,6 +25,12 @@ contract BrevisRequest is FeeVault {
         OpMode_KECCAK
     }
 
+    enum AskForType {
+        NULL,
+        QueryData,
+        Proof
+    }
+
     struct Request {
         uint256 deadline;
         uint256 fee;
@@ -37,9 +39,24 @@ contract BrevisRequest is FeeVault {
         RequestStatus status;
         Option option;
     }
-    mapping(bytes32 => Request) public requests; // TODO: store hash of request data to save gas cost
 
-    event RequestTimeoutUpdated(uint256 from, uint256 to);
+    struct RequestExt {
+        uint256 canChallengeBefore;
+        AskForType askFor;
+        uint256 shouldRespondBefore;
+    }
+
+    uint256 public requestTimeout;
+    uint256 public challengeWindow;
+    uint256 public responseTimeout; // BVN responses an ask-for-data request
+    IBrevisProof public brevisProof;
+    ISigsVerifier public immutable sigsVerifier;
+
+    mapping(bytes32 => Request) public requests; // TODO: store hash of request data to save gas cost
+    mapping(bytes32 => RequestExt) public requestExts;
+    mapping(bytes32 => bytes32) public keccakToMimc;
+
+    /* Events */
     event RequestSent(bytes32 requestId, address sender, uint256 fee, IBrevisApp callback, Option option);
     event RequestFulfilled(bytes32 requestId);
     event RequestsFulfilled(bytes32[] requestIds);
@@ -47,10 +64,23 @@ contract BrevisRequest is FeeVault {
     event RequestCallbackFailed(bytes32 requestId);
     event RequestsCallbackFailed(bytes32[] requestIds);
 
+    event OpRequestsFulfilled(bytes32[] requestIds, bytes[] queryURLs);
+    event AskFor(bytes32 indexed requestId, AskForType askFor, address from);
+    event QueryDataPost(bytes32 indexed requestId);
+    event ProofPost(bytes32 indexed requestId);
+
+    event RequestTimeoutUpdated(uint256 from, uint256 to);
+    event ChallengeWindowUpdated(uint256 from, uint256 to);
+    event ResponseTimeoutUpdated(uint256 from, uint256 to);
+
     constructor(address _feeCollector, IBrevisProof _brevisProof, ISigsVerifier _sigsVerifier) FeeVault(_feeCollector) {
         brevisProof = _brevisProof;
         sigsVerifier = _sigsVerifier;
     }
+
+    /*********************************
+     * External and Public Functions *
+     *********************************/
 
     function sendRequest(bytes32 _requestId, address _refundee, IBrevisApp _callback, Option _option) external payable {
         require(requests[_requestId].deadline == 0, "request already in queue");
@@ -77,7 +107,7 @@ contract BrevisRequest is FeeVault {
     ) external {
         require(!IBrevisProof(brevisProof).hasProof(_requestId), "proof already generated");
 
-        bytes32 reqIdFromProof = IBrevisProof(brevisProof).submitProof(_chainId, _proof, _withAppProof); // revert if proof is not valid
+        bytes32 reqIdFromProof = IBrevisProof(brevisProof).submitProof(_chainId, _proof, _withAppProof); // revert for invalid proof
         require(_requestId == reqIdFromProof, "requestId and proof not match");
         requests[_requestId].status = RequestStatus.ZkAttested;
 
@@ -85,10 +115,11 @@ contract BrevisRequest is FeeVault {
 
         address app = address(requests[_requestId].callback);
         if (app != address(0)) {
-            // fulfillRequest tx will succeed no matter if the callback is success or not.
-            // The relayer should set correct gas limit. If the call exceeds the gasleft(),
+            // The relayer should set correct gas limit. If the call failed due to insufficient gasleft(),
             // anyone can still call the app.brevisCallback directly to proceed
-            (bool success, ) = app.call(abi.encodeWithSelector(IBrevisApp.brevisCallback.selector, _requestId, _appCircuitOutput));
+            (bool success, ) = app.call(
+                abi.encodeWithSelector(IBrevisApp.brevisCallback.selector, _requestId, _appCircuitOutput)
+            );
             if (!success) {
                 emit RequestCallbackFailed(_requestId);
             }
@@ -113,7 +144,7 @@ contract BrevisRequest is FeeVault {
         emit RequestsFulfilled(_requestIds);
 
         if (_callback != address(0)) {
-            (bool success, )  = _callback.call(
+            (bool success, ) = _callback.call(
                 abi.encodeWithSelector(
                     IBrevisApp.brevisBatchCallback.selector,
                     _chainId,
@@ -127,7 +158,7 @@ contract BrevisRequest is FeeVault {
         }
     }
 
-    function refund(bytes32 _requestId) public {
+    function refund(bytes32 _requestId) external {
         require(block.timestamp > requests[_requestId].deadline);
         require(!IBrevisProof(brevisProof).hasProof(_requestId), "proof already generated");
         require(requests[_requestId].deadline != 0, "request not in queue");
@@ -138,66 +169,8 @@ contract BrevisRequest is FeeVault {
         emit RequestRefunded(_requestId);
     }
 
-    function setRequestTimeout(uint256 _timeout) external onlyOwner {
-        uint256 oldTimeout = requestTimeout;
-        requestTimeout = _timeout;
-        emit RequestTimeoutUpdated(oldTimeout, _timeout);
-    }
+    // --------------------- optimistic workflow functions ---------------------
 
-    enum AskForType {
-        NULL,
-        QueryData,
-        Proof
-    }
-
-    struct RequestExt {
-        uint256 canChallengeBefore;
-        AskForType askFor;
-        uint256 shouldRespondBefore;
-    }
-    mapping(bytes32 => RequestExt) public requestExts;
-    mapping(bytes32 => bytes32) public keccakToMimc;
-
-    uint256 public challengeWindow; // in seconds
-    // BVN responses an ask-for-data request
-    uint256 public responseTimeout;
-    event ChallengeWindowUpdated(uint256 from, uint256 to);
-    event ResponseTimeoutUpdated(uint256 from, uint256 to);
-
-    function setChallengeWindow(uint256 _challengeWindow) external onlyOwner {
-        uint256 oldChallengeWindow = challengeWindow;
-        challengeWindow = _challengeWindow;
-        emit ChallengeWindowUpdated(oldChallengeWindow, _challengeWindow);
-    }
-
-    function setResponseTimeout(uint256 _responseTimeout) external onlyOwner {
-        uint256 oldResponseTimeout = responseTimeout;
-        responseTimeout = _responseTimeout;
-        emit ResponseTimeoutUpdated(oldResponseTimeout, _responseTimeout);
-    }
-
-    function queryRequestStatus(bytes32 _requestId) external view returns (RequestStatus) {
-        if (
-            (requests[_requestId].status == RequestStatus.OpSubmitted ||
-                requests[_requestId].status == RequestStatus.OpQueryDataSubmitted) &&
-            requestExts[_requestId].canChallengeBefore <= block.timestamp
-        ) {
-            return RequestStatus.OpAttested;
-        }
-
-        if (
-            requests[_requestId].status == RequestStatus.OpDisputing &&
-            requestExts[_requestId].shouldRespondBefore <= block.timestamp
-        ) {
-            return RequestStatus.OpDisputed;
-        }
-
-        return requests[_requestId].status;
-    }
-
-    event OpRequestsFulfilled(bytes32[] requestIds, bytes[] queryURLs);
-
-    // Op functions
     function fulfillOpRequests(
         bytes32[] calldata _requestIds,
         bytes[] calldata _queryURLs,
@@ -223,10 +196,6 @@ contract BrevisRequest is FeeVault {
 
         emit OpRequestsFulfilled(_requestIds, _queryURLs);
     }
-
-    event AskFor(bytes32 indexed requestId, AskForType askFor, address from);
-    event QueryDataPost(bytes32 indexed requestId);
-    event ProofPost(bytes32 indexed requestId);
 
     function askForQueryData(bytes32 _requestId) external payable {
         // TODO: msg.value should be larger than a configurable value
@@ -271,12 +240,6 @@ contract BrevisRequest is FeeVault {
         }
     }
 
-    function verifyQueryDataProofAndRetrieveKeys(
-        bytes calldata _proof
-    ) private returns (bytes32 _myRequestId, bytes32 _dataHash) {
-        // TODO
-    }
-
     function askForProof(bytes32 _requestId) external payable {
         // TODO: msg.value should be larger than a configurable value
         require(
@@ -299,5 +262,58 @@ contract BrevisRequest is FeeVault {
         requests[_requestId].status = RequestStatus.ZkAttested;
 
         emit ProofPost(_requestId);
+    }
+
+    // --------------------- owner functions ---------------------
+
+    function setRequestTimeout(uint256 _timeout) external onlyOwner {
+        uint256 oldTimeout = requestTimeout;
+        requestTimeout = _timeout;
+        emit RequestTimeoutUpdated(oldTimeout, _timeout);
+    }
+
+    function setChallengeWindow(uint256 _challengeWindow) external onlyOwner {
+        uint256 oldChallengeWindow = challengeWindow;
+        challengeWindow = _challengeWindow;
+        emit ChallengeWindowUpdated(oldChallengeWindow, _challengeWindow);
+    }
+
+    function setResponseTimeout(uint256 _responseTimeout) external onlyOwner {
+        uint256 oldResponseTimeout = responseTimeout;
+        responseTimeout = _responseTimeout;
+        emit ResponseTimeoutUpdated(oldResponseTimeout, _responseTimeout);
+    }
+
+    /**************************
+     *  Public View Functions *
+     **************************/
+
+    function queryRequestStatus(bytes32 _requestId) external view returns (RequestStatus) {
+        if (
+            (requests[_requestId].status == RequestStatus.OpSubmitted ||
+                requests[_requestId].status == RequestStatus.OpQueryDataSubmitted) &&
+            requestExts[_requestId].canChallengeBefore <= block.timestamp
+        ) {
+            return RequestStatus.OpAttested;
+        }
+
+        if (
+            requests[_requestId].status == RequestStatus.OpDisputing &&
+            requestExts[_requestId].shouldRespondBefore <= block.timestamp
+        ) {
+            return RequestStatus.OpDisputed;
+        }
+
+        return requests[_requestId].status;
+    }
+
+    /*********************
+     * Private Functions *
+     *********************/
+
+    function verifyQueryDataProofAndRetrieveKeys(
+        bytes calldata _proof
+    ) private returns (bytes32 _myRequestId, bytes32 _dataHash) {
+        // TODO
     }
 }
