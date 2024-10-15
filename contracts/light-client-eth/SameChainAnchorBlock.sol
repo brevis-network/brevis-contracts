b7b40b457d2efcb2cf04aad8b6300b1dca55c4f4
jinyao <1650091775@qq.com>
2024-10-15 15:19:08 +0800

    remove useless import

--- contracts/light-client-eth/SameChainAnchorBlock.sol
+++ contracts/light-client-eth/SameChainAnchorBlock.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import "./interfaces/IAnchorBlocks.sol";
-import "../safeguard/BrevisAccess.sol";
 
 contract SameChainAnchorBlocks is IAnchorBlocks {
    function blocks(uint256 blockNum) external view returns (bytes32) {
