a7c41a1d29ebdde489e5865b3274865b2bf834e1
liuxiao <1241368737@qq.com>
2024-10-24 13:25:10 +0800

    update proof verifier / app agg (#23)
    * update proof verifier
    
    * update
    
    * attest aggregation verifying key hash
    
    * contracts deployment
    
    ---------
    
    Co-authored-by: jinyao <1650091775@qq.com>

---
+++ deploy/sdk/003_plonky2_agg_verifier.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { verify } from '../utils/utils';
+
+dotenv.config();
+
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const args:string[] = [];
+  const deployment = await deploy('Plonky2AggProofVerifier', {
+    from: deployer,
+    log: true,
+    args: args
+  });
+  await verify(hre, deployment, args);
+};
+
+deployFunc.tags = ['Plonky2AggProofVerifier'];
+deployFunc.dependencies = [];
+export default deployFunc;
