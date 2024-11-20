2f88bae97146785bec4237bfadd895e96ffdbea3
jinyao <1650091775@qq.com>
2024-11-20 14:03:10 +0800

    contracts deployments

--- deploy/sdk/001_brevis_request.ts
+++ deploy/sdk/001_brevis_request.ts
@@ -11,7 +11,7 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deployer } = await getNamedAccounts();
 
   const proof = await deployments.get('BrevisProof');
-  const sigsVerifier = await deployments.get('SigsVerifier');
+  const sigsVerifier = await deployments.get('BrevisProof');
   const args = ["0x58b529F9084D7eAA598EB3477Fe36064C5B7bbC1", proof.address, sigsVerifier.address];
   const deployment = await deploy('BrevisRequest', {
     from: deployer,
