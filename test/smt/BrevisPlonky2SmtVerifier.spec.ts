6ca384e69e9af9ad67d610c4c479adc1d14dbe7e
liuxiao <1241368737@qq.com>
2025-01-15 12:13:10 +0800

    update plonky2 smt proof verifier (#31)
    * update smt
    
    * update
    
    * update

--- test/smt/BrevisPlonky2SmtVerifier.spec.ts
+++ test/smt/BrevisPlonky2SmtVerifier.spec.ts
@@ -4,6 +4,7 @@ import { ethers, waffle } from 'hardhat';
 import { BrevisPlonky2SmtVerifier } from '../../typechain';
 import assert from 'assert';
 import { hexToBytes } from '../util';
+import {expect} from "chai";
 
 async function deployContract(admin: Wallet) {
   const _factory = await ethers.getContractFactory('BrevisPlonky2SmtVerifier');
@@ -33,37 +34,75 @@ describe('BrevisPlonky2SmtVerifier test', async () => {
   it('should pass on true proof', async () => {
     const result = await contract.verifyProof(
         [
-          BigNumber.from('0x07e2778626964a754b9195def728ea19cf0e3d8b8c5753ba43bc0dd139133736'), // a0
-          BigNumber.from('0x042438dd97b6592229539cd15443ee5f9d63b31db2f31481c65594711f3067d2'), // a1
+          BigNumber.from('0x1a6de85e1829528a0963b409da663343c00c138f7582f990d52ff19b5ff5f29a'), // a0
+          BigNumber.from('0x218988b65ffad99a70aab1c35e7be3d66565370b07e37723db4734ed3aac5c6d'), // a1
 
-          BigNumber.from('0x236453f47b140a9a05cbe55415e48974299241a2c69469cc63da70e62bc320c4'), // b00
-          BigNumber.from('0x28f5c16d64ed7784a8a2f3595362ddc8949848b5ca58820d3309e216fc725e57'), // b01
-          BigNumber.from('0x284c0ec5df51b61bac50ec39b335c911b9ede6c8a08aff8f415ae566d59c78e6'), // b10
-          BigNumber.from('0x168970a94c85b5a679aaf149141485ac1e2cf3100133c73f47cd299ffa8ae992'), // b11
+          BigNumber.from('0x19e44d5e50d7ae28d1496263fb6b5ec2202cc6816d9edde31df6c78c9622d152'), // b00
+          BigNumber.from('0x11681f4302e49dfa1ffd8875b3edc9b00fcf09bfba9b6b3fe946bcde308b753d'), // b01
+          BigNumber.from('0x1b9a79b676a5cbe3966298a987bdd4489816ae2595759afbe65eee7a593a26c8'), // b10
+          BigNumber.from('0x269aacd368910231b4e38034a91d74dc28a7443a8a7c25df97dd478f99e4ced2'), // b11
 
-          BigNumber.from('0x22ff39d4822505452f1668fe7d3e75702007f6a81bc64c1615880756144a6a19'), // c0
-          BigNumber.from('0x17065d881740ce2efd4d9494d691cdb52b229a0316d92a751a1d73a9a18c41a2') // c1
+          BigNumber.from('0x08eb00a0bff2d2f28313e41f0ef2441da2218bb271eb9a3d267f9825421574b8'), // c0
+          BigNumber.from('0x2aa727997830b038b0c5a24f3720cafa900df677c1100642cd764a2bbf89f994') // c1
         ],
         [
-          BigNumber.from('0x1019ebea4a5e5e32c3e9bf00424f3043e29173d00798b616b17a0e1e59994e09'), // Commitment 0
-          BigNumber.from('0x153f42fa8353f93f61ea7ae6eca63e917d4cbf38dcf1afd8301828ae42f67ee7') // Commitment 1
+          BigNumber.from('0x1e98f5bf21991b908775d2e7ac6558410fe5f67a14464b2a8699e51cdb0adb25'), // Commitment 0
+          BigNumber.from('0x0b0f63cbccf76a457c986722457e5209fd6c5676bfcd15359117a327637e39b9') // Commitment 1
         ],
         [
-          BigNumber.from('0x0ed223b3b6edc2c3610fd8f865e1a9f910597a49d0882da3753f125fad74b764'), // Commitment POK0
-          BigNumber.from('0x1b4d6fff41d141d1fde2abfe75dcb60291cfb97bac4215b23e7d0041d6b76df7') // Commitment POK1
+          BigNumber.from('0x27ad93d284d74b3d09c341f205136699a95c22b7e80a0869af593106c4fb0aa0'), // Commitment POK0
+          BigNumber.from('0x10b18b34c2a68dbacd9d008d933350c68431d32bbec17bf741e1b16fa2dc63c7') // Commitment POK1
         ],
         [
-          BigNumber.from('0x00000000000000000000000000000000409f3590dcd221e5786a20423cfc8305'), // EndBlkHash 0
-          BigNumber.from('0x00000000000000000000000000000000738a88d3efaa6210428de2024f75aceb'), // EndBlkHash 1
-          BigNumber.from('0x0000000000000000000000000000000045187e590779d8dfca61be4ea6c9e888'), // NewSmtRoot 0
-          BigNumber.from('0x0000000000000000000000000000000031b07e7bfefbda1cdb66921cb0d63b86'), // NewSmtRoot 1
-          BigNumber.from('0x00000000000000000000000000000000ca3022d33d006b913ecfad08eb9d6dde'), // OldSmtRoot 0
-          BigNumber.from('0x00000000000000000000000000000000985e93fdd80ca6fadebcd2172d28c3f3'), // OldSmtRoot 1
-          BigNumber.from('0x0000000000000000000000000000000082fc73fc57007f802f958f4b18d7fb6f'), // NextChunkMerkleRoot 0
-          BigNumber.from('0x000000000000000000000000000000009f01498afa81edea7ee5404bdb09a283'), // NextChunkMerkleRoot 1
-          BigNumber.from('0x1ba0ce182fc9cf0eba3aa35b990b052d0d956d641defdddd8b93473a7d9d6d5e')  // CircuitDigest
+          BigNumber.from('0x000000000000000000000000000000004fcf04370e7fff28a61aeb865faa4c97'), // EndBlkHash 0
+          BigNumber.from('0x00000000000000000000000000000000201c06eddd475833ba42138333ffc3dd'), // EndBlkHash 1
+          BigNumber.from('0x00000000000000000000000000000000a7c945bf248170650ea0370c8bd4dea8'), // NewSmtRoot 0
+          BigNumber.from('0x00000000000000000000000000000000301ede52931e4d12a2f0e7a0b4698b6f'), // NewSmtRoot 1
+          BigNumber.from('0x00000000000000000000000000000000b244c559122805487f8377c4ad9bc212'), // OldSmtRoot 0
+          BigNumber.from('0x000000000000000000000000000000000c34b373ad27a41840940db61ec3c5b7'), // OldSmtRoot 1
+          BigNumber.from('0x0000000000000000000000000000000000000000000000000000000000000000'), // NextChunkMerkleRoot 0
+          BigNumber.from('0x0000000000000000000000000000000000000000000000000000000000000000'), // NextChunkMerkleRoot 1
+          BigNumber.from('0x25d8769b8e3aa1755bc55f71443ae0f9f0b5b3263d51de80ec1c4cddd16dff20')  // CircuitDigest
         ]
     );
     assert.equal(result, true);
   });
+
+    it('should revert on false proof', async () => {
+        const result = contract.verifyProof(
+            [
+                BigNumber.from('0x1a6de85e1829528a0963b409da663343c00c138f7582f990d52ff19b5ff5f29a'), // a0
+                BigNumber.from('0x218988b65ffad99a70aab1c35e7be3d66565370b07e37723db4734ed3aac5c6d'), // a1
+
+                BigNumber.from('0x19e44d5e50d7ae28d1496263fb6b5ec2202cc6816d9edde31df6c78c9622d152'), // b00
+                BigNumber.from('0x11681f4302e49dfa1ffd8875b3edc9b00fcf09bfba9b6b3fe946bcde308b753d'), // b01
+                BigNumber.from('0x1b9a79b676a5cbe3966298a987bdd4489816ae2595759afbe65eee7a593a26c8'), // b10
+                BigNumber.from('0x269aacd368910231b4e38034a91d74dc28a7443a8a7c25df97dd478f99e4ced2'), // b11
+
+                BigNumber.from('0x08eb00a0bff2d2f28313e41f0ef2441da2218bb271eb9a3d267f9825421574b8'), // c0
+                BigNumber.from('0x2aa727997830b038b0c5a24f3720cafa900df677c1100642cd764a2bbf89f994') // c1
+            ],
+            [
+                BigNumber.from('0x1e98f5bf21991b908775d2e7ac6558410fe5f67a14464b2a8699e51cdb0adb25'), // Commitment 0
+                BigNumber.from('0x0b0f63cbccf76a457c986722457e5209fd6c5676bfcd15359117a327637e39b9') // Commitment 1
+            ],
+            [
+                BigNumber.from('0x27ad93d284d74b3d09c341f205136699a95c22b7e80a0869af593106c4fb0aa0'), // Commitment POK0
+                BigNumber.from('0x10b18b34c2a68dbacd9d008d933350c68431d32bbec17bf741e1b16fa2dc63c7') // Commitment POK1
+            ],
+            [
+                BigNumber.from('0x000000000000000000000000000000004fcf04370e7fff28a61aeb865faa4c97'), // EndBlkHash 0
+                BigNumber.from('0x00000000000000000000000000000000201c06eddd475833ba42138333ffc3dd'), // EndBlkHash 1
+                BigNumber.from('0x00000000000000000000000000000000a7c945bf248170650ea0370c8bd4dea8'), // NewSmtRoot 0
+                BigNumber.from('0x00000000000000000000000000000000301ede52931e4d12a2f0e7a0b4698b6f'), // NewSmtRoot 1
+                BigNumber.from('0x00000000000000000000000000000000b244c559122805487f8377c4ad9bc212'), // OldSmtRoot 0
+                BigNumber.from('0x000000000000000000000000000000000c34b373ad27a41840940db61ec3c5b7'), // OldSmtRoot 1
+                BigNumber.from('0x0000000000000000000000000000000000000000000000000000000000000001'), // NextChunkMerkleRoot 0
+                BigNumber.from('0x0000000000000000000000000000000000000000000000000000000000000000'), // NextChunkMerkleRoot 1
+                BigNumber.from('0x25d8769b8e3aa1755bc55f71443ae0f9f0b5b3263d51de80ec1c4cddd16dff20')  // CircuitDigest
+            ]
+        );
+
+        await expect(result).reverted;
+    });
 });
