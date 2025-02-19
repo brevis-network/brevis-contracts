ccace2786312a7412bb29230357a880d9543d118
jinyao <1650091775@qq.com>
2025-02-19 15:13:32 +0800

    contract deployment

--- deploy/eth-beacon/001_EthereumLightClient.ts
+++ deploy/eth-beacon/001_EthereumLightClient.ts
@@ -17,9 +17,9 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     '0x4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95',
     [0, 74240, 144896, 194048, 269568],
     ['0x00000000', '0x01000000', '0x02000000', '0x03000000', '0x04000000',],
-    11075584, // period finalized slot
-    '0xb549827c59146c49262326cbc4747602e1dc45ab6af675fa01c80a47f8a676e8', // period sha root
-    '0x23fe301686e7607e52750afd172da048840af65eac8e9cc246a677d0085e8812', // period poseidon root
+    11083876, // finalized slot period = 1353
+    '0xb549827c59146c49262326cbc4747602e1dc45ab6af675fa01c80a47f8a676e8', // period 1353 sha root
+    '0x23fe301686e7607e52750afd172da048840af65eac8e9cc246a677d0085e8812', // period 1353 poseidon root
     verifier.address
   ];
 
