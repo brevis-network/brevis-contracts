0b5b5e04b1defa0896d92f8de9e5a281648954f4
Xiaozhou Li <xli@celer.network>
2025-02-19 21:42:18 -0800

    add governed owner

---
+++ contracts/safeguard/governed-owner/GovernedOwnerProxy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.18;
+
+import "./proxies/CommonOwnerProxy.sol";
+import "./proxies/UpgradeableOwnerProxy.sol";
+
+contract GovernedOwnerProxy is
+    CommonOwnerProxy,
+    UpgradeableOwnerProxy
+{
+    constructor(address _initializer) OwnerProxyBase(_initializer) {}
+}
