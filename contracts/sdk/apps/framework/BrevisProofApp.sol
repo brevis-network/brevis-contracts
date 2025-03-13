eb0d7122fa10259269256516c4614781f95e98aa
Xiaozhou Li <xl@cs.princeton.edu>
2025-03-13 11:05:48 -0700

    update BrevisProofApp template

--- contracts/sdk/apps/framework/BrevisProofApp.sol
+++ contracts/sdk/apps/framework/BrevisProofApp.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// App that directly interact with the BrevisProof contract.
+// App that directly interact with the BrevisProof contract.B
 abstract contract BrevisProofApp {
     IBrevisProof public brevisProof;
 
-    function _checkBrevisProof(bytes calldata _proof, bytes calldata _appOutput, bytes32 _appVkHash) internal {
-        (, bytes32 appCommitHash, bytes32 appVkHash) = brevisProof.submitProof(uint64(block.chainid), _proof);
+    function _checkBrevisProof(
+        uint64 _chainId,
+        bytes calldata _proof,
+        bytes calldata _appOutput,
+        bytes32 _appVkHash
+    ) internal {
+        (, bytes32 appCommitHash, bytes32 appVkHash) = brevisProof.submitProof(_chainId, _proof);
         require(appVkHash == _appVkHash, "mismatch vkhash");
         require(appCommitHash == keccak256(_appOutput), "invalid circuit output");
     }
@@ -25,10 +30,10 @@ abstract contract BrevisProofApp {
 interface IBrevisProof {
     struct ProofData {
         bytes32 commitHash;
-        bytes32 vkHash;
         bytes32 appCommitHash; // zk-program computing circuit commit hash
         bytes32 appVkHash; // zk-program computing circuit Verify Key hash
         bytes32 smtRoot;
+        bytes32 dummyInputCommitment; // zk-program computing circuit dummy input commitment
     }
 
     function submitProof(
